cbd7ab9b33a3cb8ab7a40618efcf6ccdf5ae6ed5
Daniel Del Rio <daniel.delrio@obf.ateneo.edu>
2021-06-09 21:04:15 +0800

    Finish footer

--- components/Article.vue
+++ components/Article.vue
@@ -1,116 +1,125 @@
-<template> 
-    <div>
-        <v-navigation-drawer permanent width="120" app>
+<template>
+  <div>
+    <v-navigation-drawer permanent width="120" app>
       <v-list>
-          <v-list-item class="px-2">
-            <v-list-item-avatar>
-              <v-img src="https://randomuser.me/api/portraits/women/85.jpg"></v-img>
-            </v-list-item-avatar>
-            
-          </v-list-item>
-
+        <v-list-item class="px-2">
+          <v-list-item-avatar>
+            <v-img
+              src="https://randomuser.me/api/portraits/women/85.jpg"
+            ></v-img>
+          </v-list-item-avatar>
+        </v-list-item>
       </v-list>
-  </v-navigation-drawer>
-        <div class="hero">
-            <v-img 
-            src="https://via.placeholder.com/720x480"
-            width="100vw"
-            height="500px"
-            lazy-src="https://via.placeholder.com/720x480"
-            >
-                <v-container class="h-100">
-                    <div class="h-100 d-flex justify-start align-center">
-                        <div>
-                            <h1>{{ articleTitle }}</h1>
-                            <h3>{{ articleAuthor }}</h3>
-                            <p>{{ articleShortDesc }}</p>
-                        </div>
-                    </div>
-                </v-container>
-            </v-img>
-        </div>
-
-        <!-- article content goes here -->
-        <v-container>
-            <slot></slot>
-
-            <div class="d-flex justify-space-between align-center">
-                <nuxt-link :to="`/${prevArticleSlug}`" class="article-controls d-flex flex-row">
-                    <Arrow direction='left' :color=themeColor />
-                    <div class="d-flex justify-center align-start flex-column ml-2">
-                        <span :style="`color: ${themeColor}`">previous</span>
-                        <span>{{ prevArticle }}</span>
-                    </div>
-                </nuxt-link>
-
-                <nuxt-link :to="`/${nextArticleSlug}`" class="article-controls d-flex flex-row-reverse">
-                    <Arrow direction='right' :color=themeColor />
-                    <div class="d-flex justify-center align-end flex-column mr-2">
-                        <span :style="`color: ${themeColor}`">next</span>
-                        <span>{{ nextArticle }}</span>
-                    </div>
-                </nuxt-link>
+    </v-navigation-drawer>
+    <div class="hero">
+      <v-img
+        src="https://via.placeholder.com/720x480"
+        width="100vw"
+        height="500px"
+        lazy-src="https://via.placeholder.com/720x480"
+      >
+        <v-container class="h-100">
+          <div class="h-100 d-flex justify-start align-center">
+            <div>
+              <h1>{{ articleTitle }}</h1>
+              <h3>{{ articleAuthor }}</h3>
+              <p>{{ articleShortDesc }}</p>
             </div>
+          </div>
         </v-container>
+      </v-img>
     </div>
+
+    <!-- article content goes here -->
+    <v-container>
+      <slot></slot>
+
+      <div class="d-flex justify-space-between align-center">
+        <nuxt-link
+          :to="`/${prevArticleSlug}`"
+          class="article-controls d-flex flex-row"
+        >
+          <Arrow direction="left" :color="themeColor" />
+          <div class="d-flex justify-center align-start flex-column ml-2">
+            <span :style="`color: ${themeColor}`">previous</span>
+            <span>{{ prevArticle }}</span>
+          </div>
+        </nuxt-link>
+
+        <nuxt-link
+          :to="`/${nextArticleSlug}`"
+          class="article-controls d-flex flex-row-reverse"
+        >
+          <Arrow direction="right" :color="themeColor" />
+          <div class="d-flex justify-center align-end flex-column mr-2">
+            <span :style="`color: ${themeColor}`">next</span>
+            <span>{{ nextArticle }}</span>
+          </div>
+        </nuxt-link>
+      </div>
+    </v-container>
+
+    <Footer :color="themeColor"></Footer>
+  </div>
 </template>
 
 <script>
-import Arrow from './Arrow'
+import Arrow from "./Arrow";
+import Footer from "./Footer";
 
 export default {
-    components: {
-        Arrow
-    },
-    props:[
-        'themeColor',
-        'articleTitle',
-        'articleAuthor',
-        'articleShortDesc',
-        'prevArticle',
-        'prevArticleSlug',
-        'nextArticle',
-        'nextArticleSlug'
-    ]
-}
+  components: {
+    Arrow,
+    Footer,
+  },
+  props: [
+    "themeColor",
+    "articleTitle",
+    "articleAuthor",
+    "articleShortDesc",
+    "prevArticle",
+    "prevArticleSlug",
+    "nextArticle",
+    "nextArticleSlug",
+  ],
+};
 </script>
 
 <style lang="scss" scoped>
 .hero {
-    h1 {
-        font-family: $heading-font-bold;
-        font-size: 4em;
-    }
+  h1 {
+    font-family: $heading-font-bold;
+    font-size: 4em;
+  }
 
-    h3 {
-        font-family: $subheading-font;
-        font-weight: 600;
-        font-size: 1.5em;
-    }
+  h3 {
+    font-family: $subheading-font;
+    font-weight: 600;
+    font-size: 1.5em;
+  }
 
-    p {
-        font-family: $subheading-font;
-        font-size: 1.25em;
-    }
+  p {
+    font-family: $subheading-font;
+    font-size: 1.25em;
+  }
 }
 
 .article-controls {
-
-    text-decoration: none;
-    span {
-        &:first-child {
-            font-size: 0.75em;
-            text-transform: uppercase;
-            font-family: $subheading-font;
-            font-weight: 600;
-        }
-
-        &:last-child {
-            font-size: 1.13em;
-            color: #000000;
-            font-family: $body-font;
-            font-weight: 700;
-        }
+  text-decoration: none;
+  span {
+    &:first-child {
+      font-size: 0.75em;
+      text-transform: uppercase;
+      font-family: $subheading-font;
+      font-weight: 600;
     }
+
+    &:last-child {
+      font-size: 1.13em;
+      color: #000000;
+      font-family: $body-font;
+      font-weight: 700;
+    }
+  }
 }
 </style>
---
+++ components/Footer.vue
@@ -0,0 +1,61 @@
+<template>
+  <footer class="footer" :style="cssVars">
+    <img
+      :src="require('../assets/images/footer/guidon-logo.png')"
+      alt="The GUIDON"
+      class="guidon-logo"
+    />
+    <h1>ABOUT THE INTERACTIVE</h1>
+    <p>
+      A collaboration project by the Inquiry and Features Staffs for AY
+      2020-2021<br />
+      Interactive designed and developed by Daniel Del Rio, Harvey Jay Sison,
+      Miggy Pinaroc, Patricia Gabrielle Ramos, and Samson Elijah Tan
+    </p>
+  </footer>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: ["color"],
+  computed: {
+    cssVars() {
+      return {
+        "--color": this.color,
+      };
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.footer {
+  background-color: var(--color);
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  padding: 3.5em;
+  text-align: center;
+
+  h1 {
+    font-family: $heading-font;
+    font-size: 0.9em;
+    color: #f4f4f4;
+    margin-top: 1.5em;
+    margin-bottom: 1em;
+  }
+
+  p {
+    font-size: 0.9em;
+    color: #f4f4f4;
+  }
+}
+
+.guidon-logo {
+  width: 250px;
+  height: 100%;
+}
+</style>
